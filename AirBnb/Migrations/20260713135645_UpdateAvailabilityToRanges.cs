1666273c0f74f308a9697710aa8c3a7b2e3e9d23
PPala10 <ppala23@ku.edu.tr>
2026-07-13 16:58:13 +0300

    Availability date attribute seperated to start and end date.

---
+++ AirBnb/Migrations/20260713135645_UpdateAvailabilityToRanges.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirBnb.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAvailabilityToRanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "date",
+                table: "Availabilities",
+                newName: "start_date");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "end_date",
+                table: "Availabilities",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "end_date",
+                table: "Availabilities");
+
+            migrationBuilder.RenameColumn(
+                name: "start_date",
+                table: "Availabilities",
+                newName: "date");
+        }
+    }
+}
