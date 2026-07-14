dba908e12381eaa9eac08da5097b12bb07894110
PPala10 <ppala23@ku.edu.tr>
2026-07-14 11:57:17 +0300

    Review db content migration.

---
+++ AirBnb/Migrations/20260714085650_AddReviewsRelation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirBnb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewsRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
