9ecc4d43c96b0ff19ab7c02ea2bc553580f8c5ed
PPala10 <ppala23@ku.edu.tr>
2026-07-28 14:51:45 +0300

    Migration ListingImage

---
+++ AirBnb/Migrations/20260728112800_AddImageListToListing.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AirBnb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageListToListing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ListingImages",
+                columns: table => new
+                {
+                    listingImageId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    listingId = table.Column<int>(type: "integer", nullable: false),
+                    imagePath = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListingImages", x => x.listingImageId);
+                    table.ForeignKey(
+                        name: "FK_ListingImages_Listings_listingId",
+                        column: x => x.listingId,
+                        principalTable: "Listings",
+                        principalColumn: "listId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ListingImages_listingId",
+                table: "ListingImages",
+                column: "listingId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListingImages");
+        }
+    }
+}
