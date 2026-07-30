db50b08c62f1432cd53351c3538b7a99502a197c
PPala10 <ppala23@ku.edu.tr>
2026-07-30 11:35:38 +0300

    migration for http deploy

--- AirBnb/Program.cs
+++ AirBnb/Program.cs
@@ -32,6 +32,12 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
